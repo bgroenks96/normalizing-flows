ff5f28061d59c80f03df55f716efcb3b8bc90d6b
Brian Groenke <brian.groenke@colorado.edu>
2019-11-18 10:21:54 -0700

    Fully specify flow length in ctor

--- flows/flow.py
+++ flows/flow.py
@@ -3,13 +3,12 @@ import tensorflow_probability as tfp
 from .base_transform import BaseTransform
 
 class Flow():
-    def __init__(self, transform: BaseTransform):
-        self.transforms = [transform]
-
-    def __call__(self, x):
-        assert isinstance(x, BaseTransform)
-        self.transforms.append(x)
-        return self
+    def __init__(self, num_flows, transform_init):
+        assert num_flows > 0, "num_flows must be > 0"
+        self.num_flows = num_flows
+        self.transforms = [transform_init(i) for i in range(num_flows)]
+        self.type = type(self.transforms[0])
+        assert all([self.type == type(t) for t in self.transforms]), "All transforms should have the same type"
 
     @tf.function
     def forward(self, z, args: tf.Tensor):
@@ -20,7 +19,7 @@ class Flow():
         z    : (batch_size, d)
         args : (batch_size, m) where m is equal to the total number of parameters for all flows
         """
-        n_flows, n_params = self.num_flows(), self.param_count(tf.shape(z)[1])
+        n_flows, n_params = self.num_flows, self.param_count(tf.shape(z)[1])
         args = tf.reshape(args, (-1, n_flows, n_params // n_flows))
         z_k = z
         ldj = 0.0
@@ -31,6 +30,3 @@ class Flow():
 
     def param_count(self, d):
         return sum([t.param_count(d) for t in self.transforms])
-
-    def num_flows(self):
-        return len(self.transforms)
