Fix bugs in ILDJ computation

flows/glow/act_norm.py
@@ -4,8 +4,8 @@ import numpy as np
 from .regularized_bijector import RegularizedBijector
 
 class ActNorm(RegularizedBijector):
-    def __init__(self, alpha=1., init_from_data=False, name='act_norm',
-                 forward_min_event_ndims=1, inverse_min_event_ndims=1,
+    def __init__(self, alpha=0.1, init_from_data=False, name='act_norm',
+                 forward_min_event_ndims=3, inverse_min_event_ndims=3,
                  *args, **kwargs):    
         """
         Creates a new activation normalization (actnorm) bijector.
@@ -45,12 +45,11 @@ class ActNorm(RegularizedBijector):
         
     def _inverse_log_det_jacobian(self, x):
         self._init_vars(x)
-        fldj = tf.math.reduce_sum(self.log_s)
-        #print(self.name, fldj)
-        return tf.broadcast_to(fldj, (x.shape[0],1))
+        ildj = tf.math.reduce_sum(self.log_s)
+        return tf.broadcast_to(ildj, (x.shape[0],))
     
     def _forward_log_det_jacobian(self, y):
-        return -self._forward_log_det_jacobian(y)
+        return -self._inverse_log_det_jacobian(y)
     
     def _regularization_loss(self):
         return self.alpha*tf.math.reduce_sum(self.log_s**2)

flows/glow/affine_coupling.py
@@ -10,23 +10,19 @@ def resnet_glow(hidden_dims=512, kernel_size=3, alpha=1.0E-3):
         x = Input((None,None,c//2))
         h_1 = Conv2D(hidden_dims, kernel_size, padding='same', kernel_regularizer=l2(alpha))(x)
         h_1 = Activation('relu')(h_1)
-        h_1 = BatchNormalization()(h_1)
         h_2 = Conv2D(hidden_dims, 1, padding='same', kernel_regularizer=l2(alpha))(h_1)
         h_2 = Activation('relu')(h_2)
-        h_2 = BatchNormalization()(h_2)
         s = Conv2D(c//2, kernel_size, padding='same', kernel_regularizer=l2(alpha), kernel_initializer='zeros')(h_2)
         s = Lambda(lambda x: x+2)(s)
         s = Activation('sigmoid')(s)
         t = Conv2D(c//2, kernel_size, padding='same', kernel_regularizer=l2(alpha), kernel_initializer='zeros')(h_2)
         model = Model(inputs=x, outputs=[s, t])
-        # note that loss and optimizer don't matter for individual coupling models
-        #model.compile(loss='kld', optimizer='adam')
         return model
     return _resnet
 
 class AffineCoupling(RegularizedBijector):
     def __init__(self, nn_ctor=resnet_glow(),
-                 forward_min_event_ndims=1, inverse_min_event_ndims=1,
+                 forward_min_event_ndims=3, inverse_min_event_ndims=3,
                  *args, **kwargs):
         super().__init__(forward_min_event_ndims=forward_min_event_ndims,
                          inverse_min_event_ndims=inverse_min_event_ndims,
@@ -54,13 +50,12 @@ class AffineCoupling(RegularizedBijector):
         x_b = y_b
         return tf.concat([x_a, x_b], axis=-1)
     
-    def _forward_log_det_jacobian(self, y):
+    def _inverse_log_det_jacobian(self, y):
         self._init_nn(y)
         _, y_b = tf.split(y, 2, axis=-1)
         s, _ = self.nn(y_b)
-        ildj = -tf.math.reduce_sum(tf.math.log(s), axis=[1,2,3])
-        #print(self.name, ildj)
-        return tf.expand_dims(ildj, axis=-1)
+        ildj = tf.math.reduce_sum(tf.math.log(s), axis=[1,2,3])
+        return ildj
     
     def _regularization_loss(self):
         assert self.nn is not None, 'bijector not initialized'

flows/glow/invertible_conv_lu.py
@@ -5,8 +5,8 @@ import scipy
 from .regularized_bijector import RegularizedBijector
 
 class InvertibleConv(RegularizedBijector):
-    def __init__(self, alpha=1.0E-2, name='invertible_1x1_conv',
-                 forward_min_event_ndims=0, inverse_min_event_ndims=0,
+    def __init__(self, alpha=0.1, name='invertible_1x1_conv',
+                 forward_min_event_ndims=3, inverse_min_event_ndims=3,
                  *args, **kwargs):
         super().__init__(*args,
                          forward_min_event_ndims=forward_min_event_ndims,
@@ -68,12 +68,11 @@ class InvertibleConv(RegularizedBijector):
     
     def _inverse_log_det_jacobian(self, x):
         self._init_vars(x)
-        fldj = tf.math.reduce_sum(self.log_d)
-        #print(self.name, -fldj)
-        return tf.squeeze(tf.broadcast_to(fldj, (x.shape[0],1)))
+        ildj = tf.math.reduce_sum(self.log_d)
+        return tf.broadcast_to(ildj, (x.shape[0],))
 
     def _forward_log_det_jacobian(self, y):
-        return -self._forward_log_det_jacobian(y)
+        return -self._inverse_log_det_jacobian(y)
     
     def _regularization_loss(self):
         return self.alpha*tf.math.reduce_sum(self.log_d**2)

flows/glow/tests/test_glow.py
@@ -5,7 +5,7 @@ import numpy as np
 import itertools as it
 import pytest
 
-@pytest.mark.parametrize('layer', [0,1,2])
+@pytest.mark.parametrize('layer', [1,2,3])
 def test_step_forward_inverse(layer):
     normal_diag = tfp.distributions.MultivariateNormalDiag(loc=np.zeros((1,256,), dtype=np.float32),
                                                            scale_diag=np.ones((1,256,), dtype=np.float32))
@@ -18,7 +18,7 @@ def test_step_forward_inverse(layer):
     x_ = glow._inverse(y)
     np.testing.assert_array_almost_equal(x_, x_reshaped, decimal=5)
     
-@pytest.mark.parametrize(['num_layers', 'depth_per_layer'], list(it.product(range(3), range(4))))
+@pytest.mark.parametrize(['num_layers', 'depth_per_layer'], list(it.product(range(1,4), range(1,5))))
 def test_flow_forward_inverse(num_layers, depth_per_layer):
     normal_diag = tfp.distributions.MultivariateNormalDiag(loc=np.zeros((1,256,), dtype=np.float32),
                                                            scale_diag=np.ones((1,256,), dtype=np.float32))

models/glow.py
@@ -3,21 +3,58 @@ import tensorflow_probability as tfp
 from flows.glow import GlowFlow, RegularizedBijector
 
 class Glow(tf.Module):
-    def __init__(self, prior_distribution: tfp.distributions.Distribution,
-                num_layers=1, depth_per_layer=1,
-                optimizer=tf.keras.optimizers.Adam(lr=1.0E-4),
-                *glow_args, **glow_kwargs):
+    def __init__(self,
+                 prior,
+                 prior_parameterize=None,
+                 num_layers=1, depth_per_layer=1,
+                 optimizer=tf.keras.optimizers.Adam(lr=1.0E-3),
+                 clip_grads=True,
+                 *glow_args, **glow_kwargs):
+        """
+        Creates a new Glow model for variational inference.
+        
+        prior : either a tfp.distributions.Distribution instance that represents the variational prior,
+                or a function f: (X -> Theta) where X is the model inputs and Theta is a tensor of distribution parameters.
+                In the latter case, prior_parameterize must also be provided.
+        prior_parameterize : a function f: (Theta -> tfp.distributions.Distribution) which parameterizes a distribution
+                             from the parameter values produced by 'prior'
+        num_layers : number of "layers" in the Glow flow; see Kingma and Dhariwal 2018
+        depth_per_layer : number of Glow steps per layer; see Kingma and Dhariwal 2018
+        optimizer : optimizer to use during training
+        clip_grads : True if gradients should be clipped to the global norm, False otherwise
+        glow_args : additional arguments for GlowFlow
+        glow_kwargs : additional keyword arguments for GlowFlow
+        """
+        if not isinstance(prior, tfp.distributions.Distribution):
+            assert prior_parameterize is not None, 'prior_parameterize must be provided for conditional priors'
         self.glow = GlowFlow(num_layers=num_layers, depth=depth_per_layer, *glow_args, **glow_kwargs)
-        self.prior = prior_distribution
-        self.target_dist = self.glow(prior_distribution)
+        self.prior = prior
+        self.prior_parameterize = prior_parameterize
         self.optimizer = optimizer
+        self.clip_grads = clip_grads
         self.reg_modules = None
         
     def train_batch(self, x):
+        """
+        Performs a single iteration of mini-batch SGD on input x.
+        Returns loss, nll, prior, ildj[, grad_norm]
+                where loss is the total optimized loss (including regularization),
+                nll is the averaged negative log likelihood component,
+                prior is the averaged prior negative log likelihodd,
+                ildj is the inverse log det jacobian,
+                and, if clip_grads is True, grad_norm is the global max gradient norm
+        """
         with tf.GradientTape() as tape:
+            if isinstance(self.prior, tfp.distributions.Distribution):
+                prior = self.prior
+            else:
+                prior_params = self.prior(x)
+                prior = self.prior_parameterize(prior_params)
             z = self.glow.inverse(x)
-            ildj = self.glow.inverse_log_det_jacobian(x, event_ndims=self.prior.event_shape.rank)
-            prior_log_probs = self.prior.log_prob(z)
+            prior_log_probs = tf.math.reduce_sum(prior.log_prob(z), axis=[1,2,3])
+            ildj = self.glow.inverse_log_det_jacobian(x, event_ndims=x.shape.rank-1)
+            # clip ILDJ to have same norm as prior log probs; this is for optimization purposes only!
+            ildj = tf.clip_by_norm(ildj, tf.norm(prior_log_probs))
             log_probs = prior_log_probs + ildj
             if not self.reg_modules:
                 self.reg_modules = [b for b in self.submodules if isinstance(b, RegularizedBijector)]
@@ -25,9 +62,13 @@ class Glow(tf.Module):
             reg_loss = [b._regularization_loss() for b in self.reg_modules]
             total_loss = nll_loss + tf.math.reduce_sum(reg_loss)
             gradients = tape.gradient(total_loss, self.trainable_variables)
-            gradients, grad_norm = tf.clip_by_global_norm(gradients, 1.0)
+            if self.clip_grads:
+                gradients, grad_norm = tf.clip_by_global_norm(gradients, 1.0)
             self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-        return total_loss, nll_loss, ildj, grad_norm
+        if self.clip_grads:
+            return total_loss, nll_loss, -tf.math.reduce_mean(prior_log_probs), tf.math.reduce_mean(ildj), grad_norm
+        else:
+            return total_loss, nll_loss, -tf.math.reduce_mean(prior_log_probs), tf.math.reduce_mean(ildj)
             
     def train(self, data):
         for x_batch in data: