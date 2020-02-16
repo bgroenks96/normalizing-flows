af6df5de515f0629db892a6effc780e606fb8a5d
Brian Groenke <brian.groenke@colorado.edu>
2020-02-16 06:18:18 +0000

    Use Wasserstein adversarial training in JFLVM

--- normalizing_flows/models/joint_flvm.py
+++ normalizing_flows/models/joint_flvm.py
@@ -8,6 +8,14 @@ from tqdm import tqdm
 from . import adversarial
 
 class JointFlowLVM(tf.Module):
+    """
+    Flow-based latent variable model for joint distribution inference.
+    Given random variables X, Y; JointFlowLVM attempts to learn a latent variable
+    model P(X,Y,Z) enabling conditional inference P(X|Y) and P(Y|X) via implicit
+    integration over the shared latent variables Z. This is done by learning two
+    bijective mappings X<->Z and Y<->Z via maximum likelihood in conjunction with
+    adversarial losses on P(X) and P(Y). See AlignFlow (Grover et al. 2019) for details.
+    """
     def __init__(self,
                  G_zx: Transform,
                  G_zy: Transform,
@@ -16,10 +24,11 @@ class JointFlowLVM(tf.Module):
                  prior: tfp.distributions.Distribution=tfp.distributions.Normal(loc=0.0, scale=1.0),
                  input_shape=None,
                  num_bins=None,
-                 optimizer_g=tf.keras.optimizers.Adam(lr=1.0E-4),
-                 optimizer_dx=tf.keras.optimizers.Adam(lr=1.0E-4),
-                 optimizer_dy=tf.keras.optimizers.Adam(lr=1.0E-4),
-                 clip_grads=1.0,
+                 adversarial_loss_ctor=adversarial.wasserstein_loss,
+                 optimizer_g=tf.keras.optimizers.Adam(lr=1.0E-4, beta_1=0.5, beta_2=0.9),
+                 optimizer_dx=tf.keras.optimizers.Adam(lr=1.0E-4, beta_1=0.5, beta_2=0.9),
+                 optimizer_dy=tf.keras.optimizers.Adam(lr=1.0E-4, beta_1=0.5, beta_2=0.9),
+                 clip_grads=10.0,
                  name='flvm'):
         super().__init__(name=name)
         self.G_zx = G_zx
@@ -29,6 +38,7 @@ class JointFlowLVM(tf.Module):
         self.prior = prior
         self.input_shape = input_shape
         self.num_bins = num_bins
+        self.loss_ctor = adversarial_loss_ctor
         self.optimizer_g = optimizer_g
         self.optimizer_dx = optimizer_dx
         self.optimizer_dy = optimizer_dy
@@ -42,45 +52,41 @@ class JointFlowLVM(tf.Module):
             
     def initialize(self, input_shape):
         self.input_shape = input_shape
+        self.Dx_loss, self.Gx_loss = self.loss_ctor(self.D_x)
+        self.Dy_loss, self.Gy_loss = self.loss_ctor(self.D_y)
         with tf.init_scope():
             self.G_zx.initialize(input_shape)
             self.G_zy.initialize(input_shape)
-            
+    
     def _preprocess(self, x):
         if self.num_bins is not None:
             x += tf.random.uniform(x.shape, 0, 1./self.num_bins)
         return x
     
-    def _eval_likelihood(self, transform, x):
-        z, ldj = transform.inverse(x)
-        prior_log_probs = self.prior.log_prob(z)
-        if prior_log_probs.shape.rank > 1:
-            # reduce log probs along non-batch dimensions
-            prior_log_probs = tf.math.reduce_sum(prior_log_probs, axis=[i for i in range(1,z.shape.rank)])
-        return prior_log_probs + ldj
+    def predict_y(self, x):
+        z, _ = self.G_zx.inverse(x)
+        y, _ = self.G_zy.forward(z)
+        return y
+    
+    def predict_x(self, y):
+        z, _ = self.G_zy.inverse(y)
+        x, _ = self.G_zx.forward(z)
+        return x
         
     @tf.function
-    def eval_batch(self, x, y, **flow_kwargs):
+    def eval_generators_on_batch(self, x, y):
         assert self.input_shape is not None, 'model not initialized'
         num_elements = tf.cast(x.shape[1]*x.shape[2]*x.shape[3], tf.float32)
         x = self._preprocess(x)
         y = self._preprocess(y)
-        # compute generator predictions
+        # compute generator outputs
         z_x, ildj_x = self.G_zx.inverse(x)
         y_x, _ = self.G_zy.forward(z_x)
         z_y, ildj_y = self.G_zy.inverse(y)
         x_y, _ = self.G_zx.forward(z_y)
-        # compute discriminator predictions
-        dx_pred_real = self.D_x(x)
-        dx_pred_gen = self.D_x(x_y)
-        dy_pred_real = self.D_y(y)
-        dy_pred_gen = self.D_y(y_x)
-        # compute discriminator losses
-        dx_loss = adversarial.bce_loss(dx_pred_real, dx_pred_gen)
-        dy_loss = adversarial.bce_loss(dy_pred_real, dy_pred_gen)
-        # compute generator adversarial losses (flip labels)
-        gx_loss = adversarial.bce_loss(dx_pred_gen, dx_pred_real)
-        gy_loss = adversarial.bce_loss(dy_pred_gen, dy_pred_real)
+        # compute adversarial losses
+        gx_loss = self.Gx_loss(x, x_y)
+        gy_loss = self.Gy_loss(y, y_x)
         # compute likelihood losses
         prior_logp_x = self.prior.log_prob(z_x)
         prior_logp_y = self.prior.log_prob(z_y)
@@ -88,45 +94,53 @@ class JointFlowLVM(tf.Module):
             # reduce log probs along non-batch dimensions
             prior_logp_x = tf.math.reduce_sum(prior_logp_x, axis=[i for i in range(1,prior_logp_x.shape.rank)])
             prior_logp_y = tf.math.reduce_sum(prior_logp_y, axis=[i for i in range(1,prior_logp_y.shape.rank)])
-        nll_x = -(prior_logp_x + ildj_x - self.scale_factor*num_elements) / num_elements
-        nll_y = -(prior_logp_y + ildj_y - self.scale_factor*num_elements) / num_elements
-        return nll_x, nll_y, gx_loss, gy_loss, dx_loss, dy_loss
+        nll_x = -tf.math.reduce_mean((prior_logp_x + ildj_x - self.scale_factor*num_elements) / num_elements)
+        nll_y = -tf.math.reduce_mean((prior_logp_y + ildj_y - self.scale_factor*num_elements) / num_elements)
+        return nll_x, nll_y, gx_loss, gy_loss
+    
+    @tf.function
+    def eval_discriminators_on_batch(self, x, y):
+        x_pred = self.predict_x(y)
+        y_pred = self.predict_y(x)
+        dx_loss = self.Dx_loss(x, x_pred)
+        dy_loss = self.Dy_loss(y, y_pred)
+        return dx_loss, dy_loss
         
     @tf.function
-    def train_batch(self, x, y, lam=1.0, **flow_kwargs):
+    def train_generators_on_batch(self, x, y, lam=1.0):
         assert self.input_shape is not None, 'model not initialized'
-        nll_x, nll_y, gx_loss, gy_loss, dx_loss, dy_loss = self.eval_batch(x, y, **flow_kwargs)
+        nll_x, nll_y, gx_loss, gy_loss = self.eval_generators_on_batch(x, y)
         # compute losses
         reg_losses = [self.G_zx._regularization_loss()]
         reg_losses += [self.G_zy._regularization_loss()]
         g_obj = gx_loss + gy_loss + lam*nll_x + lam*nll_y + tf.math.add_n(reg_losses)
-        g_obj = tf.math.reduce_mean(g_obj)
-        # discriminator gradient update
-        dx_loss = tf.math.reduce_mean(dx_loss)
-        dy_loss = tf.math.reduce_mean(dy_loss)
-        dx_grads = tf.gradients(dx_loss, self.D_x.trainable_variables)
-        dy_grads = tf.gradients(dy_loss, self.D_y.trainable_variables)
-        self.optimizer_dx.apply_gradients(zip(dx_grads, self.D_x.trainable_variables))
-        self.optimizer_dy.apply_gradients(zip(dy_grads, self.D_y.trainable_variables))
         # generator gradient update
         generator_variables = list(self.G_zx.trainable_variables) + list(self.G_zy.trainable_variables)
         g_grads = tf.gradients(g_obj, generator_variables)
         if self.clip_grads:
             g_grads, grad_norm = tf.clip_by_global_norm(g_grads, self.clip_grads)
         self.optimizer_g.apply_gradients(zip(g_grads, generator_variables))
-        nll_x = tf.math.reduce_mean(nll_x)
-        nll_y = tf.math.reduce_mean(nll_y)
-        return g_obj, dx_loss, dy_loss, nll_x, nll_y
+        return g_obj, nll_x, nll_y, gx_loss, gy_loss
+    
+    @tf.function
+    def train_discriminators_on_batch(self, x, y):
+        dx_loss, dy_loss = self.eval_discriminators_on_batch(x, y)
+        dx_grads = tf.gradients(dx_loss, self.D_x.trainable_variables)
+        dy_grads = tf.gradients(dy_loss, self.D_y.trainable_variables)
+        self.optimizer_dx.apply_gradients(zip(dx_grads, self.D_x.trainable_variables))
+        self.optimizer_dy.apply_gradients(zip(dy_grads, self.D_y.trainable_variables))
+        return dx_loss, dy_loss
     
     def train(self, train_data: tf.data.Dataset, steps_per_epoch, num_epochs=1, **flow_kwargs):
         train_data = train_data.take(steps_per_epoch).repeat(num_epochs)
         with tqdm(total=steps_per_epoch*num_epochs) as prog:
             hist = dict()
-            init = tf.constant(True) # init variable for data-dependent initialization
             for epoch in range(num_epochs):
                 for x,y in train_data.take(steps_per_epoch):
-                    g_obj, dx_loss, dy_loss, nll_x, nll_y  = self.train_batch(x, y, **flow_kwargs)
-                    init=tf.constant(False)
+                    # train discriminators
+                    dx_loss, dy_loss = self.train_discriminators_on_batch(x, y)
+                    # train generators
+                    g_obj, nll_x, nll_y, _, _ = self.train_generators_on_batch(x, y)
                     utils.update_metrics(hist, g_obj=g_obj.numpy(), dx_loss=dx_loss.numpy(), dy_loss=dy_loss.numpy(),
                                          nll_x=nll_x.numpy(), nll_y=nll_y.numpy())
                     prog.update(1)
@@ -137,14 +151,17 @@ class JointFlowLVM(tf.Module):
         with tqdm(total=validation_steps) as prog:
             hist = dict()
             for x,y in validation_data:
-                nll_x, nll_y, gx_loss, gy_loss, dx_loss, dy_loss  = self.eval_batch(x, y, **flow_kwargs)
+                # train discriminators
+                dx_loss, dy_loss = self.eval_discriminators_on_batch(x, y)
+                # train generators
+                nll_x, nll_y, gx_loss, gy_loss = self.eval_generators_on_batch(x, y)
                 utils.update_metrics(hist,
-                                     nll_x=tf.math.reduce_mean(nll_x).numpy(),
-                                     nll_y=tf.math.reduce_mean(nll_y).numpy(),
-                                     gx_loss=tf.math.reduce_mean(gx_loss).numpy(),
-                                     gy_loss=tf.math.reduce_mean(gy_loss).numpy(),
-                                     dx_loss=tf.math.reduce_mean(dx_loss).numpy(),
-                                     dy_loss=tf.math.reduce_mean(dy_loss).numpy())
+                                     nll_x=nll_x.numpy(),
+                                     nll_y=nll_y.numpy(),
+                                     gx_loss=gx_loss.numpy(),
+                                     gy_loss=gy_loss.numpy(),
+                                     dx_loss=dx_loss.numpy(),
+                                     dy_loss=dy_loss.numpy())
                 prog.update(1)
                 prog.set_postfix({k: v[0] for k,v in hist.items()})
                 
